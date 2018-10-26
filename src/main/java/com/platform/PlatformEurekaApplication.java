f4abd9b3ac3da68668931f4ff8e56fba6df8f58a
DESKTOP-624SR0R\xinxiang <yaoyinbing@imobpay.com>
2018-10-26 17:35:32 +0800

    update

---
+++ src/main/java/com/platform/PlatformEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class PlatformEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformEurekaApplication.class, args);
+	}
+}
